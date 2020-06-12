7c5bb83c482b0829128153a190865d6c5d760138
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2020-06-12 12:57:21 -0400

    Fixed icon "play" button

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -43,6 +43,9 @@ const Main = (props) => {
               <polygon points="10 -1.1191e-13 14 -1.1191e-13 14 21 10 21" fillRule="nonzero"/>
             </g>
           </symbol>
+          <symbol id="play-s" viewBox="0 0 19 19">
+            <path fillRule="evenodd" clipRule="evenodd" d="M0 0L19 9.5L0 19V0Z" fill="#EEE5B5"/>
+          </symbol>
         </svg>
 
       </div>
